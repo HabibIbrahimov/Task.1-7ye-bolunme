6d3592d27695fdd5cb9cbd5ae6e217b8215af62d
HabibIbrahimov <habibyi@code.edu.az>
2021-10-04 22:20:02 +0400

    Add project files.

---
+++ Task.1        7ye bolunme/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task._1___7ye_bolunme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 37;
+            if (num%7==0)
+            {
+                Console.WriteLine($"{num} 7 ye bolunur");
+            }
+            else
+            {
+                int remainder = num % 7;
+                if (remainder>3)
+                {
+                    while (num%7!=0)
+                    {
+                        num++;
+                    }
+                }
+                else
+                {
+                    while (num%7!=0)
+                    {
+                        num--;
+                    }
+                }
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
